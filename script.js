a15a3ae5e209095f89c1fad7dc189eb787d7be21
kelpklub <kelpklub@github.com>
2026-06-30 10:54:03 +0530

    added stars in bg which move ,issue- the stars move off the screen after a while

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
 const picture = document.getElementById("picture");
+
 const details = document.getElementById("details");
 const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
+
 const flag = document.getElementById("countryflag");
 const logo = document.getElementById("agencylogo");
-flag.width = 160;
-flag.border = 1;
-logo.width = 160;
-logo.border = 1;
+
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+const config = {
+    starnum: 300,
+    speedmin: 0.05,
+    speedmax: 0.2,
+    radiusmin: 0.3,
+    radiusmax: 1.5,
+    alphamin: 0.3,
+    alphamax: 0.8,
+    glowintensity: 8
+};
+
+const starColors = [
+    "#ffffff",
+    "#ffe9c4",
+    "#b5d0ff",
+    "#f7f7a8",
+    "#c4e1ff",
+    "#ffd700",
+    "#ffb347"
+];
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+let stars = [];
+let animationFrameId;
 
 let Countdown;
 let launches = [];
 let currentLaunch = 0;
 let Countrycode = "";
 
-console.log("bfr fetch");
+flag.width = 160;
+flag.border = 1;
+logo.width = 160;
+logo.border = 1;
+
+console.log("before fetch");
 
 async function getNextLaunch() {
     try {
@@ -85,11 +119,42 @@ function updateCountdown() {
     seconds.textContent = `${s}s`;
 }
 
+function initstars() {
+    for (let i = 0; i < config.starnum; i++) {
+        stars.push({
+            x: Math.random() * width,
+            y: Math.random() * height,
+            radius: 2,
+            alpha: 1,
+            vx: Math.random() * 2,
+            vy: Math.random() * 2
+        });
+    }
+}
+
+function animatestars() {
+    ctx.clearRect(0, 0, width, height);
+    for (const star of stars) {
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+        ctx.fillStyle = `rgba(255,255,255,${star.alpha})`;
+        ctx.shadowColor = "#ffff";
+        ctx.shadowBlur = config.glowintensity;
+        ctx.fill();
+
+        star.x += star.vx;
+        star.y += star.vy;
+
+    }
+    requestAnimationFrame(animatestars);
+
+}
 async function init() {
+    initstars();
     await getNextLaunch();
     updateCountdown();
-
     setInterval(getNextLaunch, 600000);
     setInterval(updateCountdown, 1000);
 }
 init();
+animatestars();
